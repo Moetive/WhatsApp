2445c00e7aec5ecbe49f89e7d187543eb283fa72
Moetive <Moetive206@gmail.com>
2023-03-20 23:09:34 -0700

    chatlist

--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,5 +1,7 @@
 import { View, Text, Image, StyleSheet } from "react-native";
-
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 const ChatListItem = ({ chat }) => {
     console.log(chat);
   return (
@@ -12,7 +14,7 @@ const ChatListItem = ({ chat }) => {
      <View style={styles.content}>
          <View style={styles.row}>
          <Text numberOfLines={1} style={styles.name}> {chat.user.name} </Text>
-         <Text style={styles.subTitle}> {chat.lastMessage.createdAt} </Text>
+         <Text style={styles.subTitle}>{dayjs(chat.lastMessage.createdAt).fromNow(true)}</Text>
         </View>
      <Text numberOfLines={2} style={styles.subTitle}>{chat.lastMessage.text}</Text>
     </View>
