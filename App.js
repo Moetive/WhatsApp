6db04b7ff4edfe16d96b17935f9a0b77295418c6
Moetive <Moetive206@gmail.com>
2023-03-23 00:03:52 -0700

    Navigation and Message Implemented

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import ChatsScreen from './src/screens/ChatsScreen'
-import ChatScreen from './src/screens/ChatScreen';
+import { StyleSheet, View } from 'react-native';
+import Navigator from './src/navigation';
 
 export default function App() {
   return (
     <View style={styles.container}>
-<ChatScreen/>
+      <Navigator />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'whitesmoke',
     justifyContent: 'center',
-    paddingVertical: 60,
   },
 });
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,53 +1,46 @@
-import {StyleSheet, Text, ImageBackground, FlatList, KeyboardAvoidingView } from "react-native";
-import Message from "../components/Message";
-import bg from "../../assets/images/BG.png"
-import messages from '../../assets/data/messages.json'
-import InputBox from '../components/InputBox/index.js'
+import { useEffect } from 'react';
+import { ImageBackground, StyleSheet, FlatList, KeyboardAvoidingView } from 'react-native';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import Message from '../components/Message';
+import InputBox from '../components/InputBox';
+
+import bg from '../../assets/images/BG.png';
+import messages from '../../assets/data/messages.json';
 
 const ChatScreen = () => {
+  const route = useRoute();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({ title: route.params.name });
+  }, [route.params.name]);
+
   return (
     <KeyboardAvoidingView
-    behavior={Platform.OS === "ios" ? "padding" : "height"}
-    style={styles.bg}
-  >
-    <ImageBackground source={bg} style={styles.bg}>
-      <FlatList
-      data={messages}
-      renderItem={({ item }) => <Message message={item} />} 
-      style={{ padding: 10 }}
-      inverted
-      />
-      <InputBox/>
-    </ImageBackground>
-  </KeyboardAvoidingView>
-  );    
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 90}
+      style={styles.bg}
+    >
+      <ImageBackground source={bg} style={styles.bg}>
+        <FlatList
+          data={messages}
+          renderItem={({ item }) => <Message message={item} />}
+          style={styles.list}
+          inverted
+        />
+        <InputBox />
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
 };
+
 const styles = StyleSheet.create({
-    container: {
-      flexDirection: "row",
-      backgroundColor: "whitesmoke",
-      padding: 5,
-      alignItems: "center",
-    },
-    input: {
-      fontSize: 18,
-  
-      flex: 1,
-      backgroundColor: "white",
-      padding: 5,
-      paddingHorizontal: 10,
-      marginHorizontal: 10,
-  
-      borderRadius: 50,
-      borderWidth: StyleSheet.hairlineWidth,
-      borderColor: "lightgray",
-    },
-    send: {
-      backgroundColor: "royalblue",
-      padding: 7,
-      borderRadius: 15,
-      overflow: "hidden",
-    },
-  });
+  bg: {
+    flex: 1,
+  },
+  list: {
+    padding: 10,
+  },
+});
 
 export default ChatScreen;
