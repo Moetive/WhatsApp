40911310db010775f795204d8eccad746929deea
Moetive <Moetive206@gmail.com>
2023-04-02 02:36:32 -0700

    Back End Auth

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import Navigator from './src/navigation';
-import { Amplify } from 'aws-amplify'
-import awsconfig from './src/aws-exports'
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from "aws-amplify-react-native";
+import awsconfig from './src/aws-exports';
 
 Amplify.configure(awsconfig)
 
-export default function App() {
+ function App() {
   return (
     <View style={styles.container}>
       <Navigator />
@@ -23,3 +24,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+export default withAuthenticator(App);
